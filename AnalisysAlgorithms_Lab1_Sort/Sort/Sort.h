72138a53a532a0ab0a55e7bb6141236d5656f6ed
paziy_anastasiya <paziy_anastasiya@mail.ru>
2019-10-05 00:41:16 +0300

    added sort func, working on tests

---
+++ AnalisysAlgorithms_Lab1_Sort/Sort/Sort.h
@@ -0,0 +1,16 @@
+//#pragma once
+//
+////template<typename T>
+////bool comp(const T& a, const T& b);
+//
+//template<typename T>
+//void swap(T& a, T& b);
+//
+//template <typename T, typename Compare>
+//void sort(T *first, T *last, Compare comp);
+//
+//template <typename T, typename Compare>
+//void insertion_sort(T *vec, T *last, Compare comp);
+//
+//template <typename T, typename Compare>
+//void partition(T *first, T *last, Compare comp);
---
+++ AnalisysAlgorithms_Lab1_Sort/SortUnitTest/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\Sort\Sort.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SortUnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+
+	};
+}
